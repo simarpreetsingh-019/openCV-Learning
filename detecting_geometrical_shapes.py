4e75e1b31917642cf551c24a6fcfe14a7fe898c1
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2020-05-22 03:55:54 +0530

    Update detecting_geometrical_shapes.py

--- detecting_geometrical_shapes.py
+++ detecting_geometrical_shapes.py
@@ -7,3 +7,10 @@ imgGry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret , thrash = cv2.threshold(imgGry, 240 , 255, cv2.CHAIN_APPROX_NONE)
 contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+    cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1] - 5
