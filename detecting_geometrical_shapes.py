93b7f0ef2dd7412e8bdfef987fb6538f6aff28ff
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2020-05-22 03:57:06 +0530

    conditions for triangle and rectangle,square

--- detecting_geometrical_shapes.py
+++ detecting_geometrical_shapes.py
@@ -14,3 +14,12 @@ for contour in contours:
     cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
     x = approx.ravel()[0]
     y = approx.ravel()[1] - 5
+
+    if len(approx) == 3:
+        cv2.putText( img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+    elif len(approx) == 4 :
+        x, y , w, h = cv2.boundingRect(approx)
+        aspectRatio = float(w)/h
+        print(aspectRatio)
+        if aspectRatio >= 0.95 and aspectRatio < 1.05:
+            cv2.putText(img, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
