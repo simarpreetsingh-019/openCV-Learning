78488d98b6a9a60d4573a00599eaaea1aa283a6e
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2020-05-22 03:55:15 +0530

    Update detecting_geometrical_shapes.py

--- detecting_geometrical_shapes.py
+++ detecting_geometrical_shapes.py
@@ -3,3 +3,7 @@ import cv2
 
 img = cv2.imread('shapes.PNG')
 imgGry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+ret , thrash = cv2.threshold(imgGry, 240 , 255, cv2.CHAIN_APPROX_NONE)
+contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
